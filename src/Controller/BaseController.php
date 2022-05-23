c3764e5e2335b0233cc6fa914ed3f8be18353567
sebe21bth <sebe21@student.bth.se>
2022-05-23 21:23:17 +0100

    1.0.2

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -14,8 +14,24 @@ class BaseController extends AbstractController
     public function base(): Response
     {
         
-        return $this->render('index.html.twig', [
-        
-        ]);
+        return $this->render('index.html.twig');
+    }
+
+    /**
+     * @Route("/report", name="report")
+     */
+    public function report(): Response
+    {
+
+        return $this->render('report.html.twig');
+    }
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function about(): Response
+    {
+
+        return $this->render('about.html.twig');
     }
 }
